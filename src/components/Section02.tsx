514dc93bda20589feb658bd9c9066d2c07ddcc6d
sptungG <ngoviettung154@gmail.com>
2026-01-17 21:49:37 +0700

    chore: update

--- src/components/Section02.tsx
+++ src/components/Section02.tsx
@@ -8,7 +8,7 @@ export type TSection02Props = { pType?: string; pName?: string };
 const Section02 = ({ pType, pName }: TSection02Props) => {
   return (
     <section className="relative flex max-w-[100dvw] flex-col items-center justify-center overflow-hidden max-sm:py-20 sm:pb-20">
-      <div className="mb-2 text-center text-xl text-red-700 uppercase">26/11/2024</div>
+      <div className="mb-2 text-center text-xl text-red-700 uppercase">31/01/2026</div>
       <div className={cn('mb-4 text-center font-DancingScript text-4xl font-[600] text-red-800 sm:text-6xl ')}>{pType === 'h' ? 'Lễ Vu Quy' : `Lễ Thành Hôn`} </div>
       <div className="mb-4 text-center text-xl text-red-700 uppercase">{pType === 'h' ? 'Tại nhà gái' : 'Tại nhà trai'}</div>
       <div className="mb-4 flex items-center gap-1 font-Questrial text-base leading-[1.2] sm:mb-2 sm:text-lg">
