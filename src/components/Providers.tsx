c96e56bb35256ae97ad0a4b1d3faa502a28e6ebc
sptungG <ngoviettung154@gmail.com>
2026-01-17 23:53:51 +0700

    chore: update

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,15 +1,13 @@
 import { LazyMotion, domAnimation } from 'motion/react';
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
-import { SWRConfig } from 'swr';
 
 type TProvidersProps = { children?: React.ReactNode };
 
 const Providers = ({ children }: TProvidersProps) => {
   return (
     <LazyMotion features={domAnimation}>
-      <SWRConfig>{children}</SWRConfig>
-
+      {children}
       <ToastContainer autoClose={3000} closeOnClick={false} draggable={false} icon={false} newestOnTop={false} position="top-right" stacked theme="light" />
     </LazyMotion>
   );
