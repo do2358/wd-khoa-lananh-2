fdffef9a44ff7e8763916d924b6c2eb9b0f23c46
sptungG <ngoviettung154@gmail.com>
2026-01-17 20:26:03 +0700

    chore: update

--- src/components/Section02.tsx
+++ src/components/Section02.tsx
@@ -11,7 +11,7 @@ const Section02 = ({ pType, pName }: TSection02Props) => {
       <div className="mb-2 text-center text-xl text-red-700 uppercase">26/11/2024</div>
       <div className={cn('mb-4 text-center font-DancingScript text-4xl font-[600] text-red-800 sm:text-6xl ')}>{pType === 'h' ? 'Lễ Vu Quy' : `Lễ Thành Hôn`} </div>
       <div className="mb-4 text-center text-xl text-red-700 uppercase">{pType === 'h' ? 'Tại nhà gái' : 'Tại nhà trai'}</div>
-      <div className="mb-2 flex items-center gap-1 font-Questrial text-base leading-[1.2] sm:text-lg">
+      <div className="mb-4 flex items-center gap-1 font-Questrial text-base leading-[1.2] sm:mb-2 sm:text-lg">
         <span>{pType === 'h' ? 'Nhà văn hoá thôn Hà Lỗ, Thư Lâm, Hà Nội' : 'Ngõ cây xăng khu 6, Thuỵ Lôi, Thư Lâm, Hà Nội'}</span>
       </div>
       <MotionEffect inView inViewOnce={false} slide={{ direction: 'left' }} className="sm:px-10 sm:py-5">
